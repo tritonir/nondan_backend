b1cf1e511b710a12b090c74fd3ccebc7ee5eb4f2
Al-Amin14 <alaminj824@gmail.com>
2025-08-23 17:23:10 +0600

    Api-Controllers-added

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,10 @@
 import express from "express";
 import userrouter from "./user.route.js";
+import airoute from "./ai.js";
 
 const rootRouter = express.Router();
 
-
-rootRouter.use('/user',userrouter)
+rootRouter.use('/user', userrouter)
+rootRouter.use('/ai', airoute)
 
 export default rootRouter;
-
