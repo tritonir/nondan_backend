feb1276360e8200da5a2f4115f62fe23f16f9174
ArhanTibro <arhantibro2004@gmail.com>
2025-08-24 09:51:45 +0600

    deploy_test1

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import rootRouter from "./routes/root.js";
 import { connectDB } from "./config/db.js";
+import rootRouter from "./routes/root.js";
 
 dotenv.config();
 
 const app = express();
-
-connectDB();
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
@@ -23,4 +21,9 @@ app.use(express.json());
 
 app.use("/api", rootRouter);
 
-export default app;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on port ${port}`);
+});
