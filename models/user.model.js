e50c38512cad99e02717b869feab25ba26ea0ce0
Al-Amin14 <alaminj824@gmail.com>
2025-08-24 22:53:54 +0600

    register functionally and extra api

--- models/user.model.js
+++ models/user.model.js
@@ -1,54 +1,63 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-  {
-    fullname: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-    },
-
-    password: {
-      type: String,
-      required: true,
-    },
-
-    avatar: {
-      type: String,
-      default: null,
-    },
-
-    // Main role is optional now, could be inferred from clubs
-    role: {
-      type: String,
-      enum: ["student", "club_member"],
-      default: "student",
-    },
-
-    // Array of club memberships with role per club
-    clubs: [
-      {
-        club_id: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Club",
-          required: false,
+    {
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
         },
-        clubRole: {
-          type: String,
-          enum: ["admin", "moderator", "editor", "contributor"],
-          required: false,
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
         },
-      },
-    ],
-  },
-  { timestamps: true }
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        avatar: {
+            type: String,
+            default: null,
+        },
+
+        // Main role is optional now, could be inferred from clubs
+        role: {
+            type: String,
+            enum: ["student", "admin"],
+            default: "student",
+        },
+
+        // Array of club memberships with role per club
+        clubs: [
+            {
+                club_id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Club",
+                    required: false,
+                },
+                clubRole: {
+                    type: String,
+                    enum: ["admin", "moderator", "editor", "contributor"],
+                    required: false,
+                },
+            },
+        ],
+        events: [
+            {
+                event_id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Event",
+                    required: false,
+                },
+            },
+        ],
+    },
+    { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
--- routes/event.route.js
+++ routes/event.route.js
@@ -5,6 +5,8 @@ import {
   getEvent,
   updateEvent,
   deleteEvent,
+  addEventToUser,
+  getUserEvents,
 } from "../controllers/event.controller.js";
 import userjwt from "../middlewares/user.middleware.js";
 
@@ -16,5 +18,7 @@ eventRouter.get("/:id", getEvent);
 eventRouter.post("/", userjwt, createEvent);
 eventRouter.put("/:id", userjwt, updateEvent);
 eventRouter.delete("/:id", userjwt, deleteEvent);
+eventRouter.post("/regev", userjwt, addEventToUser);
+eventRouter.post("/showstev", userjwt, getUserEvents);
 
 export default eventRouter;
